b66d4adf0fc25338d004295cba8ee171dee56ed4
Roman Novosad <roman.novosad@yahoo.com>
2024-03-28 23:16:29 +0100

    singleton tests

--- creational/singleton/src/main/java/com/github/designpatterns/Singleton.java
+++ creational/singleton/src/main/java/com/github/designpatterns/Singleton.java
@@ -1,15 +1,17 @@
 package com.github.designpatterns;
 
 /**
- Task Details:
- Objective: The objective of this task is to create a Java class that demonstrates the Singleton
- design pattern.
-
- Implementation:
- Implement a Singleton class that ensures only one instance of the class is created
- throughout the application's lifecycle. Ensure thread safety to handle concurrent access
- to the Singleton instance.
-
+ * Task Details:
+ * Objective: The objective of this task is to create a Java class that demonstrates the Singleton
+ * design pattern.
+ * <p>
+ * Implementation:
+ * Implement a Singleton class that ensures only one instance of the class is created
+ * throughout the application's lifecycle.
+ * </p>
+ * Requirements:
+ * The Singleton class should have a private constructor to prevent instantiation from outside the class.
+ * Provide a public static method that returns the instance of the Singleton class.
  */
 public class Singleton {
 }
