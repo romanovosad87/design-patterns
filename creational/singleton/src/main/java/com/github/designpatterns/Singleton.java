86f2fa9f04fdee3ca0ff70c4fb0f2ba1e19126cb
Roman Novosad <roman.novosad@yahoo.com>
2024-03-28 23:06:11 +0100

    singleton tests

--- creational/singleton/src/main/java/com/github/designpatterns/Singleton.java
+++ creational/singleton/src/main/java/com/github/designpatterns/Singleton.java
@@ -1,15 +1,31 @@
 package com.github.designpatterns;
 
 /**
- Task Details:
- Objective: The objective of this task is to create a Java class that demonstrates the Singleton
- design pattern.
-
- Implementation:
- Implement a Singleton class that ensures only one instance of the class is created
- throughout the application's lifecycle. Ensure thread safety to handle concurrent access
- to the Singleton instance.
-
+ * Task Details:
+ * Objective: The objective of this task is to create a Java class that demonstrates the Singleton
+ * design pattern.
+ * <p>
+ * Implementation:
+ * Implement a Singleton class that ensures only one instance of the class is created
+ * throughout the application's lifecycle.
+ * </p>
+ * Requirements:
+ * The Singleton class should have a private constructor to prevent instantiation from outside the class.
+ * Provide a public static method that returns the instance of the Singleton class.
  */
 public class Singleton {
+    // Private static instance variable to hold the single instance of the class
+    private static Singleton instance;
+
+    // Private constructor to prevent instantiation from outside the class
+    private Singleton() {
+    }
+
+    // Public static method to return the singleton instance
+    public static Singleton getInstance() {
+        if (instance == null) {
+            instance = new Singleton();
+        }
+        return instance;
+    }
 }
---
+++ creational/singleton/src/test/java/com/github/designpatterns/SingletonTest.java
@@ -0,0 +1,83 @@
+package com.github.designpatterns;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import lombok.SneakyThrows;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.MethodOrderer;
+import org.junit.jupiter.api.Order;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestMethodOrder;
+import java.lang.reflect.Constructor;
+import java.lang.reflect.Field;
+import java.lang.reflect.Method;
+import java.lang.reflect.Modifier;
+
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
+class SingletonTest {
+
+    private static Class<?> clazz;
+
+    @BeforeAll
+    static void beforeAll() {
+       clazz = Singleton.class;
+    }
+
+    @Test
+    @Order(1)
+    @DisplayName("field should have private static access modifier")
+    void shouldHavePrivateAccessModifier() {
+        Field[] fields = clazz.getDeclaredFields();
+        int modifiers = fields[0].getModifiers();
+        boolean isPrivate = Modifier.isPrivate(modifiers);
+        boolean isStatic = Modifier.isStatic(modifiers);
+
+        assertThat(fields).hasSize(1);
+        assertThat(isPrivate).isTrue();
+        assertThat(isStatic).isTrue();
+    }
+
+    @Test
+    @Order(2)
+    @DisplayName("constructor should have private access modifier")
+    void shouldHavePrivateConstructor() {
+        Constructor<?>[] constructors = clazz.getDeclaredConstructors();
+        int modifiers = constructors[0].getModifiers();
+        boolean isPrivate = Modifier.isPrivate(modifiers);
+
+        assertThat(constructors).hasSize(1);
+        assertThat(isPrivate).isTrue();
+    }
+
+    @Test
+    @Order(3)
+    @DisplayName("should be one method with public access modifier")
+    void shouldBeOneMethod() {
+        Method[] methods = clazz.getDeclaredMethods();
+        int modifiers = methods[0].getModifiers();
+        boolean isPublic = Modifier.isPublic(modifiers);
+
+        assertThat(methods).hasSize(1);
+        assertThat(isPublic).isTrue();
+    }
+
+    @Test
+    @Order(3)
+    @DisplayName("should return the same instance")
+    @SneakyThrows
+    void shouldReturnTheSameObject() {
+        Method[] methods = clazz.getDeclaredMethods();
+        Method methodGetInstance = methods[0];
+        int modifiers = methodGetInstance.getModifiers();
+        boolean isStatic = Modifier.isStatic(modifiers);
+        Class<?> returnType = methodGetInstance.getReturnType();
+
+        Object singleton = methodGetInstance.invoke(null);
+        Object singleton2 = methodGetInstance.invoke(null);
+
+        assertThat(isStatic).isTrue();
+        assertThat(returnType).isEqualTo(Singleton.class);
+        assertThat(singleton).isNotNull().isEqualTo(singleton2);
+    }
+}
