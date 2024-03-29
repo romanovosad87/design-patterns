743607a29d412640eb916684f0430a8614011002
Roman Novosad <roman.novosad@yahoo.com>
2024-03-30 00:12:32 +0100

    composite pattern completed

--- structural/composite/src/main/java/com/github/designpatterns/Client.java
+++ structural/composite/src/main/java/com/github/designpatterns/Client.java
@@ -1,9 +1,17 @@
 package com.github.designpatterns;
 
 public class Client {
-    // Create leaf shapes
+    public static void main(String[] args) {
+        // Create leaf shapes
+        Shape circle = new Circle();
+        Shape square = new Square();
 
-    // Create composite shape
+        // Create composite shape
+        Drawing drawing = new Drawing();
+        drawing.addShape(circle);
+        drawing.addShape(square);
 
-    // Draw composite shape
+        // Draw composite shape
+        drawing.draw();
+    }
 }
